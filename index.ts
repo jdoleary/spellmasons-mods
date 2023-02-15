ac870d8e8d8cb8804af985f4fb3ccf5f2547df90
Jordan O'Leary <jdoleary@gmail.com>
2023-02-15 10:21:04 -0500

    chore: Export mods so they can be registered

---
+++ index.ts
@@ -0,0 +1,7 @@
+import type * as commonTypes from './types/types/commonTypes';
+
+import ExplosiveArcher from './explosive_archer/explosive_archer';
+
+export const mods: commonTypes.Mod[] = [
+    ExplosiveArcher
+];

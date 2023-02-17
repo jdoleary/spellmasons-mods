e008953e0cd1da830d404ff7ec6c741705ae6ed1
Jordan O'Leary <jdoleary@gmail.com>
2023-02-17 13:26:41 -0500

    copy: Update undead blade mod description

--- undead_blade/undead_blade.ts
+++ undead_blade/undead_blade.ts
@@ -95,7 +95,7 @@ const spell: Spell = {
 const mod: Mod = {
     modName: 'Undead Blade',
     author: 'Jordan O\'Leary',
-    description: 'Adds a new spell',
+    description: 'A spell that does lots of damage to summons and resurrected units',
     screenshot: 'spellmasons-mods/undead_blade/spellIconUndeadBlade.png',
     spells: [
         spell

33beb896410e65f64c8f48f218fbdc4fc8ccb681
Jordan O'Leary <jdoleary@gmail.com>
2023-03-20 16:25:19 -0400

    type: Update types

--- types/network/wsPieSetup.d.ts
+++ types/network/wsPieSetup.d.ts
@@ -1,3 +1,3 @@
 import { Overworld } from '../Overworld';
-export declare function joinRoom(overworld: Overworld, _room_info?: {}): Promise<void>;
+export declare function joinRoom(overworld: Overworld, _room_info?: {}, isHosting?: boolean): Promise<void>;
 export declare function setupPieAndUnderworld(): void;

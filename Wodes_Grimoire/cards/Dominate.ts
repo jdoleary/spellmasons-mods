fdec27322125f21c758f48e767ba872f80cab162
BloodSpartan <gkt1998@gmail.com>
2023-04-26 15:49:11 -0400

    Wode's Grimoire: Clean up Dominate code
    Removing outdated comments and unneeded timouts

--- Wodes_Grimoire/cards/Dominate.ts
+++ Wodes_Grimoire/cards/Dominate.ts
@@ -8,12 +8,10 @@ const {
 } = globalThis.SpellmasonsAPI;
 const { refundLastSpell } = cards;
 const Unit = globalThis.SpellmasonsAPI.Unit;
-const { oneOffImage, playDefaultSpellSFX } = cardUtils;
+const { playDefaultSpellSFX } = cardUtils;
 const { CardCategory, probabilityMap, CardRarity } = commonTypes;
 
 const cardId = 'Dominate';
-//const animationPath = 'owoWIP'; //TODO
-//const imageName = 'spellmasons-mods/Wodes_grimoire/IconWIP.png'; //TODO
 const healthThreshhold = .25;
 const spell: Spell = {
     card: {
@@ -25,28 +23,18 @@ const spell: Spell = {
         expenseScaling: 2.5,
         probability: probabilityMap[CardRarity.RARE],
         thumbnail: 'spellmasons-mods/Wodes_grimoire/graphics/icons/spelliconDominate.png',
-        //animationPath,
-        sfx: 'suffocate', //TODO
+        sfx: 'suffocate',
         description: [`Converts an enemy to fight for you if they are below ${healthThreshhold*100}% health.`], //Wololo
         effect: async (state, card, quantity, underworld, prediction) => {
-            let animationDelaySum = 0;
             //Living units, Units below threshhold, and units that are in your faction
             const targets = state.targetedUnits.filter(u => u.alive && u.health <= u.healthMax * healthThreshhold && !(u.faction = state.casterUnit.faction));
-            // Note: quantity loop should always be INSIDE of the targetedUnits loop
-            // so that any quantity-based animations will play simultaneously on multiple targets
-            // but sequentially within themselves (on a single target, e.g. multiple hurts over and over)
             for (let unit of targets) {
                 if (!prediction && !globalThis.headless) {
-                    setTimeout(() => {
                         playDefaultSpellSFX(card, prediction);
                         for (let unit of targets) {
-                            //const spellEffectImage = oneOffImage(unit, animationPath, containerSpells);
-                            setTimeout(() => {
                                 //Does spell effect for client
                                 Unit.changeFaction(unit, state.casterUnit.faction);
-                            }, 100)
                         }
-                    }, animationDelaySum)
                 } else {
                     for (let unit of targets) {
                         //Does spell effect for underworld
@@ -58,12 +46,7 @@ const spell: Spell = {
             if (targets.length == 0) {
                 refundLastSpell(state, prediction, 'No low health targets to convert, mana refunded');
             }
-            //Resolves animations for client
-            if (!prediction && !globalThis.headless) {
-                await new Promise((resolve) => {
-                    setTimeout(resolve, animationDelaySum);
-                })
-            }
+            //No animations to resolve
             return state;
         },
     },

a8c3cfda555d675b67b072ef40094da03b403278
BloodSpartan <gkt1998@gmail.com>
2023-04-26 06:05:44 -0400

    Wode's Grimoire: Add particles to Flamestrike
    PLEASE TEST THIS IN BUILD
    The api need to include emitters for this to work
    This also changes the code to something similar to bleed.
    Flamestrike will now do the full emitter lifetime before letting others spells continue

--- Wodes_Grimoire/cards/FlameStrike.ts
+++ Wodes_Grimoire/cards/FlameStrike.ts
@@ -1,20 +1,21 @@
 /// <reference path="../../globalTypes.d.ts" />
 import type { Spell } from '../../types/cards/index';
+import { makeFlameStrikeWithParticles } from '../graphics/ParticleCollection';
 
 const {
     cardUtils,
     commonTypes,
     PlanningView,
+    cards,
 } = globalThis.SpellmasonsAPI;
 
 const { drawUICircle } = PlanningView;
 const Unit = globalThis.SpellmasonsAPI.Unit;
-const { oneOffImage, playDefaultSpellSFX } = cardUtils;
+const { playDefaultSpellSFX } = cardUtils;
+const { refundLastSpell } = cards;
 const { CardCategory, probabilityMap, CardRarity } = commonTypes;
 
 const cardId = 'FlameStrike';
-//const animationPath = 'owoWIP'; //TODO
-//const imageName = 'spellmasons-mods/Wodes_grimoire/IconWIP.png'; //TODO
 const damageMain = 40;
 const damageSplash = 10;
 const splashRadius = 64; //Is also Collison mesh * 2 (64)
@@ -28,36 +29,36 @@ const spell: Spell = {
         expenseScaling: 1.5,
         probability: probabilityMap[CardRarity.UNCOMMON],
         thumbnail: 'spellmasons-mods/Wodes_grimoire/graphics/icons/spelliconFlameStrike.png',
-        //animationPath,
-        sfx: 'burst', //TODO
+        sfx: 'burst',
         description: [`Deals ${damageMain} damage to the target and ${damageSplash} damage to nearby targets in a small area.`],
         effect: async (state, card, quantity, underworld, prediction) => {
-            let animationDelaySum = 0;
-            let delayBetweenAnimations = 400;
+            //Using await new Promise instead of the other way spells work to force
+            //Flamestrike to finish before continue to the next spell.
+            //There MUST be a resolve for the spell to continue
+            await new Promise<void>((resolve) => {
             //Living units
             const targets = state.targetedUnits.filter(u => u.alive);
             const adjustedRadius = splashRadius + state.aggregator.radius;
-            // Note: quantity loop should always be INSIDE of the targetedUnits loop
-            // so that any quantity-based animations will play simultaneously on multiple targets
-            // but sequentially within themselves (on a single target, e.g. multiple hurts over and over)
+            if (targets.length == 0) {
+                refundLastSpell(state, prediction);
+                resolve();
+            } 
             for (let q = 0; q < quantity; q++) {
                 if (!prediction && !globalThis.headless) {
-                    setTimeout(() => {
-                        playDefaultSpellSFX(card, prediction);
-                        for (let unit of targets) {
-                            const explosionTargets = underworld.getUnitsWithinDistanceOfTarget(unit, adjustedRadius, prediction);
-                            //const spellEffectImage = oneOffImage(unit, animationPath, containerSpells); //figure this our
-                            setTimeout(() => {
-                                //Does spell effect for client
-                                Unit.takeDamage(unit, damageMain, state.casterUnit, underworld, prediction, state);
-                                explosionTargets.splice(1).forEach(unit => {
-                                    Unit.takeDamage(unit, damageSplash, undefined, underworld, prediction, state);
-                                });
-                            }, 100)
-                        }
-                    }, animationDelaySum)
-                       //This exsists soley for the fact itll make the effect state wait for code to finish before moving on.
-                        animationDelaySum += delayBetweenAnimations;
+                    //Does spell effect for client
+                    playDefaultSpellSFX(card, prediction);
+                    for (let unit of targets) {
+                        const explosionTargets = underworld.getUnitsWithinDistanceOfTarget(unit, adjustedRadius, prediction);
+                        setTimeout(() =>{
+                            //This setTimoue exsist to delay the damage dealt until it matches more with animation
+                            Unit.takeDamage(unit, damageMain, state.casterUnit, underworld, prediction, state);
+                            explosionTargets.splice(1).forEach(unit => {
+                            Unit.takeDamage(unit, damageSplash, undefined, underworld, prediction, state);
+                            });
+                        }, 400);
+                        //This lasts 2.5 seconds
+                        makeFlameStrikeWithParticles(unit, prediction, resolve);
+                    }
                 } else {
                     for (let unit of targets) {
                         //Does spell effect for underworld
@@ -72,14 +73,11 @@ const spell: Spell = {
                     }
                 }
             }
-            //No refund because theres no special filter.
-            //Resolves animations for client
-            if (!prediction && !globalThis.headless) {
-                await new Promise((resolve) => {
-                    setTimeout(resolve, animationDelaySum);
-                })
+            if (prediction){
+                resolve();
             }
-            return state;
+            });
+        return state;
         },
     },
 };

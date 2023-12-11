45702bc2ae7f079bfc8e9bab89095bfeee1eba11
Jordan O'Leary <jdoleary@gmail.com>
2023-12-11 12:56:05 -0500

    Renes Gimmicks: Update spritesheet
    to include spell icons

--- Renes_gimmicks/Index.ts
+++ Renes_gimmicks/Index.ts
@@ -23,6 +23,6 @@ const mod: Mod = {
         Caltrops, //OnMove doesnt seem to be implemented
         //Thorns,//composeOnDamageEvents do not pass argument damageDealer right now
     ],
-    spritesheet: 'spellmasons-mods/Renes_gimmicks/graphics/icons/VampireBiteAnimation.json'
+    spritesheet: 'spellmasons-mods/Renes_gimmicks/graphics/icons/renes_spritesheet.json'
 };
 export default mod;

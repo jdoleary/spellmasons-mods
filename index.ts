4dd5d8b39592873c5d05edaeeac18d058f7a29db
Jordan O'Leary <jdoleary@gmail.com>
2024-04-21 17:28:58 -0400

    clean: Remove explosive archer mod
    This mod has been broken for a while and I don't have the time to update it

--- index.ts
+++ index.ts
@@ -1,12 +1,10 @@
 import type * as commonTypes from './types/types/commonTypes';
 
-import ExplosiveArcher from './explosive_archer/explosive_archer';
 import UndeadBlade from './undead_blade/undead_blade';
 import Wodes_Grimoire from './Wodes_Grimoire/Index';
 import Renes_Gimmicks from './Renes_gimmicks/Index';
 
 const mods: commonTypes.Mod[] = [
-    ExplosiveArcher,
     UndeadBlade,
     Wodes_Grimoire,
     Renes_Gimmicks

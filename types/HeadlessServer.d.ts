c5688dec066d81d31130c6c12be6aeb537d6110f
Jordan O'Leary <jdoleary@gmail.com>
2025-04-12 21:52:42 -0400

    types: v1.57.0

--- types/HeadlessServer.d.ts
+++ types/HeadlessServer.d.ts
@@ -1,2 +1,1 @@
 import './Shims';
-import './SpellmasonsMods.cjs.js';
---
+++ types/network/p2p/peerConf.d.ts
@@ -0,0 +1,11 @@
+export declare const simplePeerConf: {
+    iceServers: ({
+        urls: string;
+        username?: undefined;
+        credential?: undefined;
+    } | {
+        urls: string;
+        username: string;
+        credential: string;
+    })[];
+};
